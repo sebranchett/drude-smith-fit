097e754654711deb1350861ce9b631e1db5927ee
sebranchett <s.e.branchett@tudelft.nl>
2024-02-23 12:33:20 +0100

    put plotting in separate file/module

--- fit.py
+++ fit.py
@@ -1,6 +1,9 @@
 import csv
 import numpy as np
-import matplotlib.pyplot as plt
+from data_plots import plot_complex_numbers
+from data_plots import plot_frequency_vs_real_part
+from data_plots import plot_frequency_vs_imaginary_part
+from data_plots import plot_frequency_vs_magnitude
 
 
 def read_csv(filename):
@@ -21,57 +24,6 @@ def read_csv(filename):
             complex_numbers.append(complex_number)
 
     return np.array(frequencies), np.array(complex_numbers)
-    return frequencies, complex_numbers
-
-
-def plot_complex_numbers(complex_numbers):
-    real_parts = [complex_number.real for complex_number in complex_numbers]
-    imaginary_parts = [
-        complex_number.imag for complex_number in complex_numbers
-    ]
-
-    plt.scatter(real_parts, imaginary_parts)
-    plt.xlabel('Real Part')
-    plt.ylabel('Imaginary Part')
-    plt.title('Complex Numbers Plot')
-    plt.savefig('complex_numbers_plot.png')
-    plt.show()
-
-
-def plot_frequency_vs_real_part(frequencies, complex_numbers):
-    plt.plot(
-        frequencies,
-        [complex_number.real for complex_number in complex_numbers]
-    )
-    plt.xlabel('Frequency')
-    plt.ylabel('Real Part')
-    plt.title('Frequency vs Real Part')
-    plt.savefig('frequency_vs_real_part.png')
-    plt.show()
-
-
-def plot_frequency_vs_imaginary_part(frequencies, complex_numbers):
-    plt.plot(
-        frequencies,
-        [complex_number.imag for complex_number in complex_numbers]
-    )
-    plt.xlabel('Frequency')
-    plt.ylabel('Imaginary Part')
-    plt.title('Frequency vs Imaginary Part')
-    plt.savefig('frequency_vs_imaginary_part.png')
-    plt.show()
-
-
-def plot_frequency_vs_magnitude(frequencies, complex_numbers):
-    plt.plot(
-        frequencies,
-        [abs(complex_number) for complex_number in complex_numbers]
-    )
-    plt.xlabel('Frequency')
-    plt.ylabel('Magnitude')
-    plt.title('Frequency vs Magnitude')
-    plt.savefig('frequency_vs_magnitude.png')
-    plt.show()
 
 
 if __name__ == "__main__":
