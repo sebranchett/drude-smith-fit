99af1c4b463e72d4ef7ccc29e01e7dcc448e0a14
sebranchett <s.e.branchett@tudelft.nl>
2024-02-26 16:50:57 +0100

    send to Laurens

--- study_c1.py
+++ study_c1.py
@@ -1,8 +1,9 @@
 import numpy as np
 from data_plots import plot_frequency_vs_real_and_imaginary_parts
+from matplotlib import pyplot as plt
 
 
-def fit_function(frequency):
+def fit_function(frequency, c1):
     # Calculate hte Drude-Smith mobility
     e = 1.602 * 10E-19
     m0 = 9.109 * 10E-31
@@ -11,17 +12,16 @@ def fit_function(frequency):
     mstar = 0.18 * m0
     tau = 80 * 10E-15
     # c1 = -0.82
-    c1 = -0.2
 
     oneminiomegatau = 1. - (1j * omega * tau)
     p1 = e * tau / mstar
     p2 = 1. / (1. - oneminiomegatau)
     p3 = 1. + c1/(1. - oneminiomegatau)
 
-    print("Real part is zero for w^2 = ", (2. + 3*c1)/tau*tau*(c1 + 2))
-    print("c1 must be greater than ", 2*(tau*tau-1)/(3-tau*tau))
+    print("Real part is zero for w^2 = ", (2. + 5*c1)/tau*tau*(c1 + 2))
+    print("c1 must be greater than ", 2*(tau*tau-1)/(5-tau*tau))
     if c1 > 2*(tau*tau-1)/(5-tau*tau):
-        print("omega has a root in real part at", np.sqrt((2. + 3*c1)/tau*tau*(c1 + 2)))
+        print("omega has a root in real part at", np.sqrt((2. + 5*c1)/tau*tau*(c1 + 2)))
     print("Imaginary part is zero for w^2 = ",
           (6*tau*tau*(1+c1) + tau*tau*np.sqrt(36*(1+c1)*(1+c1) - 4*(1+2*c1))) /
           2*(1+2*c1))
@@ -31,7 +31,16 @@ def fit_function(frequency):
     return p1 * p2 * p3
 
 
-points = np.linspace(1, 2, 20)
-fit_result = fit_function(points)
+points = np.linspace(0.1*10E12, 0.7*10E12, 20)
 
-plot_frequency_vs_real_and_imaginary_parts(points, fit_result)
+for c1 in np.linspace(-1, 0, 11):
+    fit_result = fit_function(points, c1)
+    plt.plot(points, fit_result, label=f'c1 = {c1}')
+
+plt.xlabel('Points')
+plt.ylabel('mu_DS')
+plt.legend()
+plt.savefig('what_know.png')
+plt.show()
+
+# plot_frequency_vs_real_and_imaginary_parts(points, fit_result)
