619928038108ba50f3424015a010c16e75838c91
sebranchett <s.e.branchett@tudelft.nl>
2024-02-27 18:03:44 +0100

    two different methods give the same result

--- new_study_c1.py
+++ new_study_c1.py
@@ -3,31 +3,42 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def fit_function(frequency, e, m0, mstar, tau, c1):
+def fit_function2(frequency, e, mstar, tau, c1):
     # Calculate the Drude-Smith mobility
     factor = e * tau / mstar
-    print("factor", factor)
     omega = 2 * np.pi * frequency
+    onePlusw2t2 = 1. + (omega * omega * tau * tau)
+    oneMinusw2t2 = 1. - (omega * omega * tau * tau)
 
-    onebyonePlusw2t2 = 1. / (1. + (omega * omega * tau * tau))
-    print("onebyonePlusw2t2", onebyonePlusw2t2[0], onebyonePlusw2t2[-1])
+    result = onePlusw2t2 + c1 * oneMinusw2t2 + \
+        1j * omega * tau * (onePlusw2t2 + 2. * c1)
 
-    result = (1 - c1) + 2. * c1 * onebyonePlusw2t2 + \
-        1j * omega * tau * (1. + 2. * c1 * onebyonePlusw2t2)
-    print("result", result[0], result[-1])
-
-    return factor * onebyonePlusw2t2 * result
+    return factor * result / (onePlusw2t2*onePlusw2t2)
 
 
-e = 1.  # 1.602 * 10E-19
-m0 = 1.  # 9.109 * 10E-31
-mstar = 1.  # 0.18 * m0
-tau = 1.  # 80 * 10E-15
-points = np.linspace(0.0, 1, 10)
-c1 = -1.  # -0.82
-fit_result = fit_function(points, e, m0, mstar, tau, c1)
-plt.plot(points, fit_result.real, label='real')
-plt.plot(points, fit_result.imag, label='imag')
+def fit_function3(frequencies, e, mstar, tau, c1, c2, c3):
+    # Define your function here, using the complex argument
+    f1 = e * tau / mstar
+    f2 = 1 / (1 - 1j * 2 * np.pi * frequencies * tau)
+    f3 = 1 + (c1 / (1 - 1j * 2 * np.pi * frequencies * tau)) + \
+             (c2 / (1 - 1j * 2 * np.pi * frequencies * tau) ** 2) + \
+             (c3 / (1 - 1j * 2 * np.pi * frequencies * tau) ** 3)
+    complex_argument = f1 * f2 * f3
+    return complex_argument
+
+
+e = 1.602 * 10E-19
+m0 = 9.109 * 10E-31
+mstar = 0.18 * m0
+tau = 80 * 10E-15
+points = np.linspace(.1*10E10, .7*10E10, 21)
+c1 = -0.82
+# fit_result2 = fit_function2(points, e, mstar, tau, c1)
+# plt.plot(points, fit_result2.real, label='real2')
+# plt.plot(points, fit_result2.imag, label='imag2')
+fit_result3 = fit_function3(points, e, mstar, tau, c1, 0., 0.)
+plt.plot(points, fit_result3.real, label='real3')
+plt.plot(points, fit_result3.imag, label='imag3')
 
 plt.xlabel('Points')
 plt.ylabel('mu_DS')
