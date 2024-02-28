7b7ee6521be0d6a3c67165e26b46785b7fe589a1
sebranchett <s.e.branchett@tudelft.nl>
2024-02-28 11:39:40 +0100

    everything in one file :(

--- combine.py
+++ combine.py
@@ -1,7 +1,6 @@
 import csv
 import numpy as np
 from scipy.optimize import curve_fit
-from reproduce import drude_smith_c1
 import matplotlib.pyplot as plt
 
 
@@ -23,6 +22,38 @@ def read_csv(filename, min_frequency, max_frequency):
                 complex_numbers.append(complex_number)
 
     return np.array(frequencies), np.array(complex_numbers)
+
+
+def drude_smith_c1(frequency, m, tau, c1):
+    # Calculate the Drude-Smith mobility with only 1 c coefficient
+    e = 1.602 * 10E-19
+    m0 = 9.109 * 10E-31
+
+    mstar = m * m0
+    factor = e * tau / mstar
+    omega = 2 * np.pi * frequency
+    onePlusw2t2 = 1. + (omega * omega * tau * tau)
+    oneMinusw2t2 = 1. - (omega * omega * tau * tau)
+
+    result = onePlusw2t2 + c1 * oneMinusw2t2 + \
+        1j * omega * tau * (onePlusw2t2 + 2. * c1)
+
+    return factor * result / (onePlusw2t2*onePlusw2t2)
+
+
+def drude_smith_c3(frequencies, m, tau, c1, c2=0., c3=0.):
+    # Calculate the Drude-Smith mobility with 3 c coefficients
+    e = 1.602 * 10E-19
+    m0 = 9.109 * 10E-31
+
+    mstar = m * m0
+    f1 = e * tau / mstar
+    f2 = 1 / (1 - 1j * 2 * np.pi * frequencies * tau)
+    f3 = 1 + (c1 / (1 - 1j * 2 * np.pi * frequencies * tau)) + \
+             (c2 / (1 - 1j * 2 * np.pi * frequencies * tau) ** 2) + \
+             (c3 / (1 - 1j * 2 * np.pi * frequencies * tau) ** 3)
+    complex_argument = f1 * f2 * f3
+    return complex_argument
 
 
 def fit_function(frequencies, m, tau, c1):
