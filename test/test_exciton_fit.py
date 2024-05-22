a449012265f560f8a1d7340624fbf6a8cf57cf7a
sebranchett <s.e.branchett@tudelft.nl>
2024-05-22 17:25:17 +0200

    add check for lots of parameters to fit

--- test/test_exciton_fit.py
+++ test/test_exciton_fit.py
@@ -31,6 +31,32 @@ class ExcitonFitTestCase(unittest.TestCase):
         ])
         assert np.isclose(std_dev_fit, expected_result).all()
 
+    def test_mixed_fit(self):
+        # Test fitting a mixed model with Drude and Lorentz
+        # Not a good fit, but it should work
+        set_input_parameters(False, 1., False, False, False, False,
+                             1., False, False, False)
+        frequencies, complex_numbers = read_csv(
+            'test/test_exciton.csv', 0., 2.E13
+        )
+        num_variable_params = 8
+        fitted_complex_numbers, params_fit, std_dev_fit = perform_fit(
+            frequencies, complex_numbers, num_variable_params
+        )
+        self.assertEqual(len(params_fit), 10)
+        self.assertEqual(len(std_dev_fit), 10)
+        expected_result = np.array([
+            0.10186, 1., 1.913E-15, -1., .9999, .9999,
+            1., 4.396E-8, 8.706E7, 7.083E7
+        ])
+        assert np.isclose(params_fit, expected_result, rtol=.001).all()
+
+        expected_result = np.array([
+            2.581E2, 0., 1.276E-12, 8.937E3, 1.548E4, 1.293E4,
+            0., 2.587E1, 0., 0.
+        ])
+        assert np.isclose(std_dev_fit, expected_result, rtol=.001).all()
+
 
 if __name__ == '__main__':
     unittest.main()
